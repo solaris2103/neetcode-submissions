c3632ce7e82e4f53f15dfe57df210a5c56ecc6b8
solaris2103 <loksaipothineni@hotmail.com>
2026-07-28 07:55:21 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        var result = new int[n];
+        // result[0] = 1;
+        // for(var i = 1; i < n; i++) {
+        //     result[i] = result[i-1] * nums[i-1];
+        // }
+        // int laterResult = 1;
+        // for(var i = n-1; i >= 0; i--){
+        //     result[i] *= laterResult;
+        //     laterResult *= nums[i];
+        // }
+        // return result;
+        for(var i = 0; i < n; i++) {
+            result[i] = nums.Where((item,index) => index != i).ToArray().Aggregate((acct,val) => acct * val);
+        }
+        return result;
+    }
+}
