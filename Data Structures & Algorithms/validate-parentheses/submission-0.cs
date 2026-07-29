5c84961bf950db4fbe3b3c19a2e4bb9960e31bc5
solaris2103 <loksaipothineni@hotmail.com>
2026-07-29 14:52:12 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsValid(string s) {
+        if(s.Length%2 != 0)
+            return false;
+        var stack = new Stack<char>();
+            foreach (char c in s) 
+        {
+            if (c == '(') 
+                stack.Push(')');
+            else if (c == '[') 
+                stack.Push(']');
+            else if (c == '{') 
+                stack.Push('}');
+            else if (stack.Count == 0 || stack.Pop() != c) 
+                return false;
+        }
+        return stack.Count == 0;
+    }
+}
