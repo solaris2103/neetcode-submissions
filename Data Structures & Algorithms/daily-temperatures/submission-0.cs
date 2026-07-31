65ad8a99bdcc156f86f9317e048d9dabdd1c38e7
solaris2103 <loksaipothineni@hotmail.com>
2026-07-31 17:05:25 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        int n = temperatures.Length;
+        var result = new int[n];
+        var stack = new Stack<int>();
+
+        for (int i = 0; i < n; i++) {
+            while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()]) {
+                int prevIndex = stack.Pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
